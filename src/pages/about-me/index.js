accc2359c70549293418872fb530cd3ab19a4b34
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-07-21 21:44:56 -0700

    Updated rest of pages

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap';
 
 const title = `
 About Me
@@ -13,8 +14,12 @@ class AboutMe extends Component {
   render() {
     return (
       <Container>
-        <h1>{title}</h1>
-        <p>{text}</p>
+        <Row className="justify-content-md-center">
+          <h1 style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>{title}</h1>
+        </Row>
+        <Row className="justify-content-md-center">
+          <p>{text}</p>
+        </Row>
       </Container>
     );
   }
