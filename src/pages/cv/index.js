4f5cbe9c95dffb5a5a1aa1f9166caa59bc94da16
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-07-21 21:14:46 -0700

    Updated cv page

--- src/pages/cv/index.js
+++ src/pages/cv/index.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+
+const title = `
+Curriculum Vitae
+`;
+
+const date = `
+January 1, 2022
+`;
+
+const text = `
+Below is my CV, or you can download my CV in pdf format.
+`;
 
 class CV extends Component {
   render() {
     return (
-      <div className="CV">
-        <h1>CV</h1>
-        <p>
-          This is my CV page.
-        </p>
-      </div>
+      <Container>
+        <h1>{title}</h1>
+        <p>{date}</p>
+        <p>{text}</p>
+      </Container>
     );
   }
 }
