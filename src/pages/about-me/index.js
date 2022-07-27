765e58e25787a8499ad0beddfc7a99e333d4bd0e
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-07-26 22:01:44 -0700

    Updated the about me text

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import { Row } from 'react-bootstrap';
 
-const title = `
-About Me
-`;
+const title = 'About Me';
 
-const text = `
-This is my About Me page.
-`;
+const text = 'I am originally from the beautiful foothills of the Sierra Nevada in California. I received my undergraduate degree in International Relations from University of California Davis. And when not hitting the books, I am an avid gardener, artist, and tennis player!';
 
 class AboutMe extends Component {
   render() {
@@ -18,7 +14,7 @@ class AboutMe extends Component {
           <h1 style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>{title}</h1>
         </Row>
         <Row className="justify-content-md-center">
-          <p>{text}</p>
+          <p class="text-left">{text}</p>
         </Row>
       </Container>
     );
