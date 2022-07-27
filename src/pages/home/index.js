06236905006e4822adb78651343c41c8a8680904
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-07-26 21:54:23 -0700

    Updated home page text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,27 +3,26 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import { Row, Col } from 'react-bootstrap';
 
-const title = `
-Welcome!
-`;
+const title = 'Welcome!';
 
-const text = `
-Greek was first written in Mycenae with a script known as Linear B, which was used between about 1500 and 1200 BC. This variety of Greek is known as Mycenaean. On Crete another script, known as the Cypriot syllabary, was used to write the local variety of Greek between about 1200 and 300 BC.
-`;
+const text_p1 = 'Hi all and thank you for visiting my website! I am currently a PhD Doctoral Candidate of Political Science at University of California, Davis.';
+
+const text_2 = 'My research agenda focuses broadly on examining the causes of a variety of types of intrastate violence on the regime and societal level. My graduate work, advised primarily by Josephine Andrews and Lauren Young, focused specifically on disaggregating regime types by separating civil liberties from electoral components and exploring the variances within non-electoral components (civil society freedoms, freedom of religion, and rule of law) affect intrastate violence. In another project I am working on we test the role of ethnicity and religion as sperate identities in a novel randomized control survey experiment in India where we aim to illuminate which identity is most likely stir violence and under what conditions an identity can contribute to violence. I am also involved in projects on effects of state repression on voter behavior and trust in regimes in Zimbabwe and examining Eastern versus Western Europe Cabinet stability. Recently, I have contracted with the World Bank to consult on the Zimbabwe Hight Frequency Telephone Survey Report (ZIMTRAC) survey analysis. I have also a methodological interest in multi-dimensional scaling, unexpected event during survey design using geolocation data, and survey analysis.';
 
 class Home extends Component {
   render() {
     return (
       <Container>
-        <Row className="justify-content-md-center">
+        <Row className="justify-content-md-center" >
           <h1 style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>{title}</h1>
         </Row>
         <Row>
           <Col>
             <Image src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmVzc2lvbmFsfGVufDB8fDB8fA%3D%3D&w=1000&q=80" rounded style={{marginTop: 50, marginBottom: 50, width: '25rem'}}></Image>
           </Col>
-          <Col>
-            <p class="text-left" style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>{text}</p>
+          <Col style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>
+            <p class="text-left">{text_p1}</p>
+            <p class="text-left">{text_2}</p>
           </Col>
         </Row>
       </Container>
