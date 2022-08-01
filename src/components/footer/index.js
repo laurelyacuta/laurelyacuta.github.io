dd83e8a30918b1a341f19789d2b202ed87c31047
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-07-31 18:25:29 -0700

    Fixed footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 const year = new Date().getFullYear();
 
 class Footer extends Component {
   render() {
     return(
-      <Navbar collapseOnSelect expand="lg" bg="light" fixed="bottom">
-        <Container>
-          <Nav className="justify-content-center" style={{ flex: 1}}>
-            <Nav.Item>
-              <Nav.Link eventKey="disabled" disabled>
-                Copyright © {year} by Laurel Yacuta. Made with React.
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Container>
-      </Navbar>
+      <footer className="footer">
+        <p>Copyright © {year} by Laurel Yacuta. Made with React.</p>
+      </footer>
     );
   }
 }
